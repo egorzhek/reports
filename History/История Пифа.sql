6493081e3740a6791b731a5e42233cc4db9b53e9
Евгений <egorzhek@mail.ru>
2021-07-15 19:10:54 +0300

    История пифа и результат инвестиции

---
+++ History/История Пифа.sql
@@ -0,0 +1,68 @@
+--declare @Investor Int = 16541, @FundId Int = 17578;
+declare @Investor Int = 44762, @FundId Int = 17578;
+	
+
+SELECT
+	[Investor]  = R.[REG_1],                                             -- ИД инвестора
+	[FundId]    = R.[REG_2],                                             -- ИД ПИФа
+	[W_Date]    = T.[WIRDATE],                                           -- Дата операции (проводки)
+	[W_ID]      = T.[WIRING],                                            -- W_ID
+	[Order_NUM] = KK.[Order_NUM],                                        -- Номер заявки		                                         
+	[OperName]  = W.[NAME],                                              -- Название операции
+	[WALK]      = B2.[WALK],                                             -- Код операции
+	[TYPE]      = T.[TYPE_],                                             -- Тип операции (ввод/вывод)
+	[RATE_RUR]  = VL.[RATE],                                             -- Стоимость 1 пая
+	[Amount]    = T.[AMOUNT_] * T.[TYPE_],                               -- Количество ПИФов
+	[VALUE_RUR] = [dbo].f_Round(T.[AMOUNT_] * T.[TYPE_] * VL.[RATE], 2), -- Сумма сделки
+	[Fee] = ISNULL(KK.Fee,0)                                             -- Сумма комиссии
+FROM OD_RESTS AS R WITH(NOLOCK)
+INNER JOIN OD_TURNS AS T WITH(NOLOCK) ON T.REST=R.ID and T.IS_PLAN = 'F'
+INNER JOIN OD_WIRING AS W WITH(NOLOCK) ON W.ID = T.WIRING
+INNER JOIN OD_TURNS AS T2 WITH(NOLOCK) ON T2.WIRING = T.WIRING and T2.TYPE_ = -T.TYPE_
+INNER JOIN OD_RESTS AS E2 WITH(NOLOCK) ON E2.ID = T2.REST
+INNER JOIN OD_BALANCES AS B2 WITH(NOLOCK) ON B2.ID = E2.BAL_ACC
+
+INNER JOIN [BAL_DATA_STD].[dbo].OD_SHARES AS S WITH(NOLOCK) ON R.REG_2 = s.ID
+INNER JOIN [BAL_DATA_STD].[dbo].D_B_CONTRACTS AS C WITH(NOLOCK) ON S.ISSUER = C.INVESTOR AND C.I_TYPE = 5
+OUTER APPLY
+(
+	SELECT TOP 1
+		RT.[RATE]
+	FROM [BAL_DATA_STD].[dbo].[OD_VALUES_RATES] AS RT
+	WHERE RT.[VALUE_ID] = R.REG_2
+	AND RT.[E_DATE] >= T.WIRDATE and RT.[OFICDATE] < T.WIRDATE
+	ORDER BY
+		case when DATEPART(YEAR,RT.[E_DATE]) = 9999 then 1 else 0 end ASC,
+		RT.[E_DATE] DESC,
+		RT.[OFICDATE] DESC
+) AS VL
+OUTER APPLY
+(
+		SELECT TOP 1
+			ISNULL(SISU.ADD_SUMMA, 0) as Fee, --Сумма комиссии(надбавка) за операцию
+			ISNULL(D_ISU.NUM_DATE, ISNUlL(D_DISU.NUM_DATE,0)) as Order_NUM -- Номер заявки
+		FROM OD_STEPS AS SSS
+		INNER JOIN OD_DOCS AS DDD ON DDD.ID = SSS.DOC
+		INNER JOIN U_OP_P_ISSUE AS SISU ON SISU.DOC = DDD.ID
+
+		left join U_OP_P_DEISSUE DISU on DISU.DOC =DDD.ID
+		left join OD_DOCS D_ISU on D_ISU.ID = SISU.CLAIM
+		left join OD_DOCS D_DISU on D_DISU.ID = DISU.CLAIM
+		WHERE SSS.ID = W.O_STEP
+		/* -- нет записей таких вообще
+		UNION ALL
+		SELECT
+			ISNULL(SISU.ADD_SUMMA, 0) as Fee --Сумма комиссии(надбавка) за операцию
+		FROM OD_DOLS AS LLL WITH(NOLOCK)
+		INNER JOIN OD_DOCS AS DDD WITH(NOLOCK) ON DDD.ID = LLL.DOC
+		INNER JOIN U_OP_P_ISSUE AS SISU WITH(NOLOCK) ON SISU.DOC = DDD.ID
+		WHERE LLL.ID = W.DOL
+		*/
+) AS KK
+WHERE 
+R.BAL_ACC = 2793
+AND R.REG_2 = @FundId
+AND R.REG_1 = @Investor
+AND T.WIRING is not null
+AND B2.WALK > 0 -- проводка реализована.
+ORDER BY T.WIRDATE;
---
+++ Результат инвестиций/Результат инвестиций.sql
@@ -0,0 +1,199 @@
+-- результат инвестиций
+--Declare @Investor int = 16541, @FundId int = 17578,
+--@StartDate Date = '2007-04-20',
+--@EndDate Date = '2017-12-28'
+
+Declare @Investor int = 16541, @FundId int = 17578,
+@StartDate Date = '2009-09-19',
+@EndDate Date = '2009-09-24'
+
+Declare @SItog numeric(30,10), @AmountDayMinus_RUR numeric(30,10), @Snach numeric(30,10), @AmountDayPlus_RUR numeric(30,10),
+@InvestResult numeric(30,10);
+
+declare @MinDate date, @MaxDate date
+
+SELECT
+	@MinDate = min([Date]),
+	@MaxDate = max([Date])
+FROM
+(
+	SELECT [Date]
+	FROM [CacheDB].[dbo].[InvestorFundDate] NOLOCK
+	WHERE Investor = @Investor and FundId = @FundId
+	UNION
+	SELECT [Date]
+	FROM [CacheDB].[dbo].[InvestorFundDateLast] NOLOCK
+	WHERE Investor = @Investor and FundId = @FundId
+) AS R
+
+if @StartDate is null set @StartDate = @MinDate;
+if @StartDate < @MinDate set @StartDate = @MinDate;
+if @StartDate > @MaxDate set @StartDate = @MinDate;
+
+if @EndDate is null    set @EndDate = @MaxDate;
+if @EndDate > @MaxDate set @EndDate = @MaxDate;
+if @EndDate < @MinDate set @EndDate = @MaxDate;
+
+if @StartDate = @EndDate
+begin
+	select [Error] = 'Даты равны'
+	return;
+end
+
+BEGIN TRY
+	DROP TABLE #ResInv
+END TRY
+BEGIN CATCH
+END CATCH;
+
+
+SELECT *
+INTO #ResInv
+FROM
+(
+	SELECT *
+	FROM [CacheDB].[dbo].[InvestorFundDate] NOLOCK
+	WHERE Investor = @Investor and FundId = @FundId
+	UNION
+	SELECT *
+	FROM [CacheDB].[dbo].[InvestorFundDateLast] NOLOCK
+	WHERE Investor = @Investor and FundId = @FundId
+) AS R
+WHERE [Date] >= @StartDate and [Date] <= @EndDate
+--ORDER BY [Date]
+
+-----------------------------------------------
+-- преобразование на начальную и последнюю дату
+
+-- забыть вводы выводы на первую дату
+update #ResInv set
+	AmountDayPlus  = 0, AmountDayPlus_RUR  = 0, AmountDayPlus_USD  = 0, AmountDayPlus_EVRO  = 0,
+	AmountDayMinus = 0, AmountDayMinus_RUR = 0, AmountDayMinus_USD = 0, AmountDayMinus_EVRO = 0,
+	AmountDay = 0
+where [Date] = @StartDate
+and AmountDay <> 0 -- вводы и выводы были в этот день
+
+
+-- посчитать последний день обратно
+update #ResInv set
+	SumAmount = SumAmount - AmountDay
+where [Date] = @EndDate
+and AmountDay <> 0 -- вводы и выводы были в этот день
+
+update #ResInv set
+	VALUE_RUR = [dbo].f_Round(SumAmount * RATE, 2),
+	VALUE_USD = [dbo].f_Round(SumAmount * RATE * 1.00000/USDRATE, 2),
+	VALUE_EVRO = [dbo].f_Round(SumAmount * RATE * 1.00000/EVRORATE, 2),
+	AmountDayPlus  = 0, AmountDayPlus_RUR  = 0, AmountDayPlus_USD  = 0, AmountDayPlus_EVRO  = 0,
+	AmountDayMinus = 0, AmountDayMinus_RUR = 0, AmountDayMinus_USD = 0, AmountDayMinus_EVRO = 0,
+	AmountDay = 0
+where [Date] = @EndDate
+and AmountDay <> 0 -- вводы и выводы были в этот день
+
+-- преобразование на начальную и последнюю дату
+-----------------------------------------------
+
+-- В рублях
+
+-- Итоговая оценка инвестиций
+
+SELECT
+	@SItog = VALUE_RUR
+FROM #ResInv
+where [Date] = @EndDate
+
+SELECT
+	@Snach = VALUE_RUR
+FROM #ResInv
+where [Date] = @StartDate
+
+
+
+-- сумма всех выводов средств
+SELECT
+	@AmountDayMinus_RUR = sum(AmountDayMinus_RUR), -- отрицательное значение
+	@AmountDayPlus_RUR = sum(AmountDayPlus_RUR) 
+FROM #ResInv
+
+set @InvestResult =
+(@SItog - @AmountDayMinus_RUR) -- минус, потому что отрицательное значение
+- (@Snach + @AmountDayPlus_RUR) --as 'Результат инвестиций'
+
+--select @InvestResult -- 56794533.7800000000
+
+
+--select @Snach
+
+	declare @DateCur date, @AmountDayPlus_RURCur numeric(30,10), @AmountDayMinus_RURCur numeric(30,10), @LastDate date,
+		@SumAmountDay_RUR numeric(30,10) = 0, @Counter Int = 0, @T Int, @SumT numeric(30,10) = 0, @ResutSum numeric(30,10) = 0
+
+	declare obj_cur cursor local fast_forward for
+		-- 
+		SELECT
+			[Date], [AmountDayPlus_RUR], [AmountDayMinus_RUR]
+		FROM #ResInv
+		where ([Date] in (@StartDate, @EndDate) or [AmountDay] <> 0)
+		order by [Date]
+	open obj_cur
+	fetch next from obj_cur into
+		@DateCur, @AmountDayPlus_RUR, @AmountDayMinus_RUR
+	while(@@fetch_status = 0)
+	begin
+		set @Counter += 1;
+
+		-- начальную дату пропускаем
+		if @DateCur = @StartDate
+		begin
+			set @LastDate = @DateCur
+		end
+		else
+		begin
+			-- со второй записи определяем период
+			set @T = DATEDIFF(DAY, @LastDate, @DateCur);
+			if @DateCur = @EndDate set @T = @T + 1;
+			--select @LastDate, @DateCur, @T, @Snach, @SumAmountDay_RUR, @Counter as '@Counter', @T * (@Snach + @SumAmountDay_RUR)
+
+			set @ResutSum += @T * (@Snach + @SumAmountDay_RUR)
+			set @LastDate = @DateCur
+			set @SumAmountDay_RUR = @AmountDayPlus_RUR + @AmountDayMinus_RUR
+			set @SumT += @T;
+			
+			-- 56081083,08
+		end
+
+		fetch next from obj_cur into
+			@DateCur, @AmountDayPlus_RUR, @AmountDayMinus_RUR
+	end
+	close obj_cur
+	deallocate obj_cur
+
+	--select @ResutSum, @SumT
+
+	set @ResutSum = @ResutSum/@SumT
+
+	--select @ResutSum
+
+	select
+	@InvestResult as 'Результат инвестиций',
+	@ResutSum as 'Средневзвешенная сумма вложенных средств',
+	@InvestResult/@ResutSum * 100 as 'Доходность в %',
+	@InvestResult as 'Доходность абсолютная',
+	@StartDate as 'Дата начала',
+	@EndDate as 'Дата завершения',
+	@SumT as 'Количество дней'
+
+	SELECT
+		*
+	FROM #ResInv
+	where ([Date] in (@StartDate, @EndDate) or [AmountDay] <> 0)
+	order by [Date]
+
+
+
+BEGIN TRY
+	DROP TABLE #ResInv
+END TRY
+BEGIN CATCH
+END CATCH;
+
+
